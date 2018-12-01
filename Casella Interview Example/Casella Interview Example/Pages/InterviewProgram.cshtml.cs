65855f568e7653b64800fa436a21470a8b8a5740
Christopher Leipfert <42985312+Sandman1294@users.noreply.github.com>
2018-11-30 23:35:56 -0500

    Initial Commit of Files

---
+++ Casella Interview Example/Casella Interview Example/Pages/InterviewProgram.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace Casella_Interview_Example.Pages
+{
+    public class InterviewProgramModel : PageModel
+    {
+        public void GetFrequency()
+        {
+            
+        }
+    }
+}
